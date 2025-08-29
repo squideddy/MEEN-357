4acd719ab0b928361829ac4f628b5ea3c44ac2a3
villarrealkasey0-rgb <villarrealkasey0@gmail.com>
2025-08-29 11:27:30 -0500

    Add files via upload

---
+++ 357_dictionary.py
@@ -0,0 +1,25 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Kasey Villarreal
+# Section:      357-501
+# Assignment:   Project Phase 1
+# Date:         8/27/2025
+
+rover = {}
+rover['wheel_assembly'] = {
+    'wheel':{
+        'radius':.30,'mass':1.0},
+    'speed_reducer':{
+        'type':'reverted','diam_pinion':0.04,'diam_gear':0.07,'mass2':1.5}}
+
+    
+
+
+#rover['chassis']
+#rover['science_payload']
+#rover['power_subsys']
+ 
+print(rover['wheel_assembly']['speed_reducer']['mass2'])
+
